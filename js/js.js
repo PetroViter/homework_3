018531ec3746f026ac302388829991716fe4757a
PetroViter <116358398+PetroViter@users.noreply.github.com>
2023-02-27 15:16:36 +0200

    homework_3

---
+++ js/js.js
@@ -0,0 +1,19 @@
+// task #1
+let admin;
+let name = 'John';
+admin = name;
+alert(admin);
+
+
+// task #2
+let ourPlanet = 'Earth';
+let currentUser = 'User';
+
+// task #3
+let number = '10' + 10;
+console.log(typeof number)
+
+
+
+
+
